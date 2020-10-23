70a80c474f49ad65b2a690a6d8e64100c1a7bd34
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-23 22:04:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,8 +6,10 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.each do |key, value|
-    binding.pry
+    if name == key
+    end
   end
+  binding.pry
   
 
 
