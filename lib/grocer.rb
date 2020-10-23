3067d2e65766ebc5ac3cd1111fc4f8c284aea8c6
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-23 22:29:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,7 +10,7 @@ def find_item_by_name_in_collection(name, collection)
       key.each do |element|
         
         if element[1] == name
-          item_info >> key
+          item_info << key
         end
         
       end
